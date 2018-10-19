2ebf15ca327142f3f225307a78063878b6258ebb
DESKTOP-2E4QQHL\Админ <beastyn@gmail.com>
2018-10-19 10:31:13 +0500

    commit before HUGE refactoring

---
+++ TileVania/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+   
+    //Config
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 3f;
+    [SerializeField] float deathBouncing = 5f;
+    
+    //Cached
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
+    AudioSource myAudioSource;
+    float startingGravity;
+
+    // Use this for initialization
+    void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
